519140c39b2abe95ee61c5894e67796c0799d571
JB <berginjm@gmail.com>
2025-12-25 13:32:13 -0500

    fix

--- api/Program.cs
+++ api/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
